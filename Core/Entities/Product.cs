80fceda4a591e0918e19df0b8835d029cda4149f
Mohamed Ramadan <romio@gmail.com>
2026-06-07 18:57:06 +0300

    Adding Product Entity

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+  public required string Name { get; set; }
+  public required string Description { get; set; }
+  public int QunatityInStock { get; set; }
+  public required string Type { get; set; }
+  public required string Brand { get; set; } = null!;
+  public required string PictureUrl { get; set; } = null!;
+}
