b5161180c022f95d7e6b03aad2a5f3949ea7c673
Mohamed Ramadan <romio@gmail.com>
2026-07-31 22:06:28 +0300

    Register Form & Validation & User-Defined Input controller

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,7 @@ public class RegisterDto
   [Required]
   public string LastName { get; set; } = string.Empty;
   [Required]
+  [EmailAddress]
   public string Email { get; set; } = string.Empty;
   [Required]
   public string Password { get; set; } = string.Empty;
