b07f845e6d208b79ad2bcf26b1d184c6bcfde5b8
Mohamed Ramadan <romio@gmail.com>
2026-07-31 11:09:24 +0300

    Adding Register Cabability

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+using System.Security.Claims;
+using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(
+  SignInManager<AppUser> signInManager) : BaseApiController
+{
+  [HttpPost("register")]
+  public async Task<ActionResult> Register(RegisterDto registerDto)
+  {
+    var user = new AppUser()
+    {
+      FirstName = registerDto.FirstName,
+      LastName = registerDto.LastName,
+      Email = registerDto.Email,
+      UserName = registerDto.Email
+    };
+
+    var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+    if(!result.Succeeded) return BadRequest(result.Errors);
+    return Ok();
+  }
+
+  [Authorize]
+  [HttpPost("Logout")]
+  public async Task<ActionResult> Logout()
+  {
+    await signInManager.SignOutAsync();
+    return NoContent();
+  }
+
+  [HttpGet("user-info")]
+  public async Task<ActionResult> GetUserInfo()
+  {
+    if(User.Identity?.IsAuthenticated == false) return NoContent();
+    var user = await signInManager.UserManager.Users.FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
+    if(user == null) return Unauthorized();
+
+    return Ok(new {
+      user.FirstName,
+      user.LastName,
+      user.Email
+    });
+  }
+
+  [HttpGet]
+  public ActionResult GetAuthState()
+  {
+    return Ok(new
+    {
+      IsAuthenticated = User.Identity?.IsAuthenticated ?? false
+    });
+  }
+}
